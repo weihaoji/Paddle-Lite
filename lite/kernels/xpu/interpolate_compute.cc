6e81c5cc4c840fdaa80fee967ea20ecd5bc3a4bf
weihaoji <weihaoji@baidu.com>
2020-11-10 16:42:55 +0800

    support interpolate_scale2

--- lite/kernels/xpu/interpolate_compute.cc
+++ lite/kernels/xpu/interpolate_compute.cc
@@ -15,6 +15,7 @@
 #include "lite/kernels/xpu/interpolate_compute.h"
 #include <iostream>
 #include <memory>
+#include <vector>
 #include "lite/backends/xpu/xpu_header_sitter.h"
 #include "lite/core/op_registry.h"
 
@@ -23,78 +24,139 @@ namespace lite {
 namespace kernels {
 namespace xpu {
 
+inline std::vector<int> get_new_shape(
+    std::vector<const lite::Tensor*> list_new_shape_tensor) {
+  // get tensor from
+  std::vector<int> vec_new_shape;
+  for (size_t i = 0; i < list_new_shape_tensor.size(); ++i) {
+    auto tensor = list_new_shape_tensor[i];
+    vec_new_shape.push_back(static_cast<int32_t>(*tensor->data<int32_t>()));
+  }
+
+  return vec_new_shape;
+}
+
+template <typename T>
+inline std::vector<T> get_new_data_from_tensor(const Tensor* new_data_tensor) {
+  std::vector<T> vec_new_data;
+  auto* new_data = new_data_tensor->data<T>();
+  lite::Tensor cpu_starts_tensor;
+  vec_new_data =
+      std::vector<T>(new_data, new_data + new_data_tensor->dims().production());
+  return vec_new_data;
+}
+
+void PrepareLayout(lite::Tensor* input,
+                   lite::Tensor* out_size,
+                   std::vector<const lite::Tensor*> list_new_size_tensor,
+                   lite::Tensor* scale_tensor,
+                   lite::Tensor* output,
+                   float scale,
+                   int* out_h,
+                   int* out_w) {
+  // format NCHW
+  int n = input->dims()[0];
+  int c = input->dims()[1];
+  int in_h = input->dims()[2];
+  int in_w = input->dims()[3];
+  if (list_new_size_tensor.size() > 0) {
+    // have size tensor
+    auto new_size = get_new_shape(list_new_size_tensor);
+    *out_h = new_size[0];
+    *out_w = new_size[1];
+  } else {
+    if (scale_tensor != nullptr) {
+      auto scale_data = get_new_data_from_tensor<float>(scale_tensor);
+      scale = scale_data[0];
+    }
+    if (scale > 0) {
+      *out_h = static_cast<int>(in_h * scale);
+      *out_w = static_cast<int>(in_w * scale);
+    }
+    if (out_size != nullptr) {
+      auto out_size_data = get_new_data_from_tensor<int>(out_size);
+      *out_h = out_size_data[0];
+      *out_w = out_size_data[1];
+    }
+  }
+  output->Resize({n, c, *out_h, *out_w});
+}
+
 void BilinearInterpCompute::Run() {
   auto& param = this->Param<param_t>();
   auto& ctx = this->ctx_->As<XPUContext>();
-  auto x_dims = param.X->dims();
-  CHECK_EQ(x_dims.size(), 4);
-  int n = x_dims[0];
-  int c = x_dims[1];
-  int in_h = x_dims[2];
-  int in_w = x_dims[3];
+  lite::Tensor* X = param.X;
+  int n = X->dims()[0];
+  int c = X->dims()[1];
+  int in_h = X->dims()[2];
+  int in_w = X->dims()[3];
 
+  // optionla inputs
+  lite::Tensor* OutSize = param.OutSize;
+  auto SizeTensor = param.SizeTensor;
+  auto Scale = param.Scale;
+  // output
+  lite::Tensor* Out = param.Out;
+  // optional attributes
+  float scale = param.scale;
   int out_w = param.out_w;
   int out_h = param.out_h;
-  float scale = param.scale;
-  if (scale > 0) {
-    out_h = static_cast<int>(in_h * scale);
-    out_w = static_cast<int>(in_w * scale);
-  }
-  if (param.OutSize != nullptr) {
-    out_h = param.OutSize->data<int>()[0];
-    out_w = param.OutSize->data<int>()[1];
-  }
+  // int align_mode = param.align_mode;
+  // required attributes
   bool align_corners = param.align_corners;
-  CHECK_EQ(align_corners, 1) << "XPU only support align corners = 1";
+  PrepareLayout(X, OutSize, SizeTensor, Scale, Out, scale, &out_h, &out_w);
 
-  int r = xdnn::bilinear_interp(ctx.GetRawContext(), /* context */
-                                param.X->data<float>(),
-                                param.Out->mutable_data<float>(TARGET(kXPU)),
-                                n,
-                                c,
-                                in_h,
-                                in_w,
-                                out_h,
-                                out_w,
-                                align_corners,
-                                1);
+  int r = xdnn::interpolate2d<float>(ctx.GetRawContext(), /* context */
+                                     X->data<float>(),
+                                     Out->mutable_data<float>(TARGET(kXPU)),
+                                     n,
+                                     c,
+                                     in_h,
+                                     in_w,
+                                     out_h,
+                                     out_w,
+                                     false,
+                                     align_corners,
+                                     true);
   CHECK_EQ(r, 0);
 }
 
 void NearestInterpCompute::Run() {
   auto& param = this->Param<param_t>();
   auto& ctx = this->ctx_->As<XPUContext>();
-  auto x_dims = param.X->dims();
-  CHECK_EQ(x_dims.size(), 4);
-  int n = x_dims[0];
-  int c = x_dims[1];
-  int in_h = x_dims[2];
-  int in_w = x_dims[3];
+  lite::Tensor* X = param.X;
+  int n = X->dims()[0];
+  int c = X->dims()[1];
+  int in_h = X->dims()[2];
+  int in_w = X->dims()[3];
 
+  // optionla inputs
+  lite::Tensor* OutSize = param.OutSize;
+  auto SizeTensor = param.SizeTensor;
+  auto Scale = param.Scale;
+  // output
+  lite::Tensor* Out = param.Out;
+  // optional attributes
+  float scale = param.scale;
   int out_w = param.out_w;
   int out_h = param.out_h;
-  float scale = param.scale;
-  if (scale > 0) {
-    out_h = static_cast<int>(in_h * scale);
-    out_w = static_cast<int>(in_w * scale);
-  }
-
-  if (param.OutSize != nullptr) {
-    out_h = param.OutSize->data<int>()[0];
-    out_w = param.OutSize->data<int>()[1];
-  }
+  // required attributes
   bool align_corners = param.align_corners;
+  PrepareLayout(X, OutSize, SizeTensor, Scale, Out, scale, &out_h, &out_w);
 
-  int r = xdnn::interpolate(ctx.GetRawContext(), /* context */
-                            param.X->data<float>(),
-                            param.Out->mutable_data<float>(TARGET(kXPU)),
-                            n,
-                            c,
-                            in_h,
-                            in_w,
-                            out_h,
-                            out_w,
-                            align_corners);
+  int r = xdnn::interpolate2d<float>(ctx.GetRawContext(), /* context */
+                                     X->data<float>(),
+                                     Out->mutable_data<float>(TARGET(kXPU)),
+                                     n,
+                                     c,
+                                     in_h,
+                                     in_w,
+                                     out_h,
+                                     out_w,
+                                     true,
+                                     align_corners,
+                                     true);
+
   CHECK_EQ(r, 0);
 }
 
@@ -113,8 +175,8 @@ REGISTER_LITE_KERNEL(bilinear_interp,
     .BindInput("OutSize",
                {LiteType::GetTensorTy(TARGET(kX86), PRECISION(kInt32))})
     .BindInput("SizeTensor",
-               {LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kInt32))})
-    .BindInput("Scale", {LiteType::GetTensorTy(TARGET(kXPU))})
+               {LiteType::GetTensorTy(TARGET(kX86), PRECISION(kInt32))})
+    .BindInput("Scale", {LiteType::GetTensorTy(TARGET(kX86))})
     .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kXPU))})
     .Finalize();
 
@@ -128,7 +190,7 @@ REGISTER_LITE_KERNEL(nearest_interp,
     .BindInput("OutSize",
                {LiteType::GetTensorTy(TARGET(kX86), PRECISION(kInt32))})
     .BindInput("SizeTensor",
-               {LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kInt32))})
-    .BindInput("Scale", {LiteType::GetTensorTy(TARGET(kXPU))})
+               {LiteType::GetTensorTy(TARGET(kX86), PRECISION(kInt32))})
+    .BindInput("Scale", {LiteType::GetTensorTy(TARGET(kX86))})
     .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kXPU))})
     .Finalize();
